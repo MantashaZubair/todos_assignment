cce623af026f0cc00dff307a9ce2dce88b9025df
Mantasha Zubair <98265444+MantashaZubair@users.noreply.github.com>
2023-02-02 01:46:32 -0800

    Update ToDoForm.js
    modified

--- client/src/component/ToDoForm.js
+++ client/src/component/ToDoForm.js
@@ -10,7 +10,7 @@ const ToDoForm = () => {
         e.preventDefault();
         if (!todoState) return;
         try {
-            fetch("http://localhost:5000/db", {
+            fetch("http://localhost:5000/todos", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
@@ -30,10 +30,24 @@ const ToDoForm = () => {
     };
 
     useEffect(() => {
-        fetch("http://localhost:5000/db/get")
+        fetch("http://localhost:5000/todos/get")
             .then((response) => response.json())
             .then((data) => setItemlist(data.data));
     }, []);
+
+
+    const ondelete=(id)=>{
+      try{
+        fetch(`http://localhost:5000/todos/delete/${id}`,{
+          method:'DELETE',
+          headers:{ "Content-Type": "application/json" }
+        })
+      
+      }catch(error){
+       console.log(error)
+      }
+
+    }
 
     const inputRef = useRef(null);
     useEffect(() => {
@@ -61,13 +75,14 @@ const ToDoForm = () => {
             </form>
             <div>
                 <ol>
-                    {itemList?.map((item, index) => {
+                    {itemList.map((item,index) => {
                         return (
-                            <div key={index} className="liststyle">
-                                <li>{item.todo_name}</li>
+                            <div   className="liststyle">
+                                <li key={index}>{item.todo_name}</li>
                                 <FontAwesomeIcon
                                     icon={faDeleteLeft}
                                     className="icon"
+                                    onClick={()=>{ondelete(item.todo_id)}}
                                 />
                             </div>
                         );
@@ -79,3 +94,85 @@ const ToDoForm = () => {
 };
 
 export default ToDoForm;
+
+// import React, { useEffect, useState } from "react";
+// import { useRef } from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faDeleteLeft } from "@fortawesome/free-solid-svg-icons";
+
+// const ToDoForm = () => {
+//     const [todoState, setToDoState] = useState("");
+//     const [itemList, setItemlist] = useState([]);
+//     const onsubmitHandler = (e) => {
+//         e.preventDefault();
+//         if (!todoState) return;
+//         try {
+//             fetch("http://localhost:5000/db", {
+//                 method: "POST",
+//                 headers: { "Content-Type": "application/json" },
+//                 body: JSON.stringify({
+//                     todo_name: todoState,
+//                 }),
+//             }).then(() => {
+//                 alert("sucessfull insert");
+//                 setItemlist((itemList) => [
+//                     ...itemList,
+//                     { todo_name: todoState },
+//                 ]);
+//                 setToDoState("");
+//             });
+//         } catch (err) {
+//             console.log(err.message);
+//         }
+//     };
+
+//     useEffect(() => {
+//         fetch("http://localhost:5000/db/get")
+//             .then((response) => response.json())
+//             .then((data) => setItemlist(data.data));
+//     }, []);
+
+//     const inputRef = useRef(null);
+//     useEffect(() => {
+//         inputRef.current.focus();
+//     });
+
+//     return (
+//         <>
+//             <header>
+//                 <h1>TodoList</h1>
+//             </header>
+//             <form onSubmit={onsubmitHandler}>
+//                 <div className="inputbox">
+//                     <input
+//                         placeholder="Enter to do"
+//                         type="text"
+//                         ref={inputRef}
+//                         value={todoState}
+//                         onChange={(e) => {
+//                             setToDoState(e.target.value);
+//                         }}
+//                     />
+//                     <button type="submit">Add Task</button>
+//                 </div>
+//             </form>
+//             <div>
+//                 <ol>
+//                     {itemList?.map((item, index) => {
+//                         return (
+//                             <div key={index} className="liststyle">
+//                                 <li>{item.todo_name}</li>
+//                                 <FontAwesomeIcon
+//                                     icon={faDeleteLeft}
+//                                     className="icon"
+//                                 />
+//                             </div>
+//                         );
+//                     })}
+//                 </ol>
+//             </div>
+//         </>
+//     );
+// };
+
+// export default ToDoForm;
